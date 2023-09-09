028c1892c28bc676cb3f985058b4a82431759723
misselysea <elyseacrawford@gmail.com>
2023-09-09 15:50:19 -0400

    Updated review component

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,14 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import DynamicStars from './DynamicStars';
 import styles from './Review.module.scss';
+import * as userService from './users-service';
 
 const Review = ({ review, userReview }) => {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    // Fetch the currently authenticated user
+    const currentUser = userService.getUser();
+
+    if (currentUser && currentUser.email === review.reviewerEmail) {
+      // If the currently authenticated user is the author of the review, set the author state
+      setAuthor(currentUser);
+    }
+  }, [review.reviewerEmail]);
+
   return (
     <div className={styles.review}>
-      <h3>{review.reviewerName}</h3>
+      <h3>{author ? author.firstName : 'Anonymous'}</h3>
       <p>{review.reviewText}</p>
       <DynamicStars rating={review.rating} />
 
-    
       {userReview && (
         <div className={styles.userReview}>
           <h4>Your Review:</h4>
@@ -21,4 +34,3 @@ const Review = ({ review, userReview }) => {
 };
 
 export default Review;
-
